c92f898136756e058d6bf477c6efa85bea514f4b
samplesree <137154680+samplesree@users.noreply.github.com>
2023-07-05 13:18:13 +0530

    creds issue tomcat-deployment.groovy

--- vars/tomcat-deployment.groovy
+++ vars/tomcat-deployment.groovy
@@ -1,6 +1,6 @@
 def call(ip,warfile,creds){
    //  install ssh agent plugin
-   sshagent([creds]) {
+   sshagent(['tomcat-dev']) {
   // copy war file to tomcat-dev server
   sh "scp -o StrictHostKeyChecking=no target/${warfile} ec2-user@${ip}:/opt/apache9/webapps"
   // restart tomcat 
